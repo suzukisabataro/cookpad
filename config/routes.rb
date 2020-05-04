e150b4f6ba1d7a925a8d0d39beae6be155c3ab68
suzukisabataro <dwdwdwtjagpmdwdwx@gmail.com>
2020-05-05 06:13:43 +0900

    third comit

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :users
-  resources :recipes do
-    collection do
-      get 'search'
-    end
-  end
+  resources :recipes 
 end
